d783b7623bb3102f90792e7d0e916ef3595b1a5e
Lero <lesanchezro@est.utn.ac.cr>
2026-04-24 17:36:45 -0600

    Plantilla Result
    Se creo una plantilla para resultados con  un nuevo componente

---
+++ src/components/ResultStats.jsx
@@ -0,0 +1,36 @@
+function ResultStats() {
+  return (
+    <div className="mb-4">
+
+      <div className="alert alert-success text-center">
+        🎉 ¡Juego terminado!
+      </div>
+
+      <ul className="list-group">
+
+        <li className="list-group-item d-flex justify-content-between">
+          <span>Puntos</span>
+          <strong>8</strong>
+        </li>
+
+        <li className="list-group-item d-flex justify-content-between">
+          <span>Correctas</span>
+          <strong>8</strong>
+        </li>
+
+        <li className="list-group-item d-flex justify-content-between">
+          <span>Incorrectas</span>
+          <strong>2</strong>
+        </li>
+
+        <li className="list-group-item d-flex justify-content-between">
+          <span>Porcentaje</span>
+          <strong>80%</strong>
+        </li>
+
+      </ul>
+    </div>
+  )
+}
+
+export default ResultStats
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,5 +1,36 @@
+import CardBox from "../components/CardBox"
+import Btn from "../components/Btn"
+import ResultStats from "../components/ResultStats"
+
 function Result() {
-  return <h1>Resultados</h1>
+  return (
+    <div className="container mt-5">
+      <CardBox>
+
+        <h1 className="text-center mb-4">
+          📊 Resultados
+        </h1>
+
+        <ResultStats />
+
+        <div className="d-grid gap-2">
+
+          <Btn
+            text="Jugar Otra Vez"
+            to="/"
+            type="primary"
+          />
+
+          <Btn
+            text="Compartir Resultado"
+            type="success"
+          />
+
+        </div>
+
+      </CardBox>
+    </div>
+  )
 }
 
 export default Result
