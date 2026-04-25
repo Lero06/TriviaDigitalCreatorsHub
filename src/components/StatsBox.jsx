01d50e1815f6167d386f3017ab64c7eece70492b
Lero <lesanchezro@est.utn.ac.cr>
2026-04-25 14:26:30 -0600

    Modificaciones en StatBox
    Se modifico para que sea mas grande

--- src/components/StatsBox.jsx
+++ src/components/StatsBox.jsx
@@ -1,9 +1,21 @@
 function StatsBox() {
   return (
-    <div className="d-flex justify-content-between mb-3">
-      <span className="badge bg-primary">Pregunta 1/10</span>
-      <span className="badge bg-warning text-dark">⏱ 15s</span>
-      <span className="badge bg-success">Puntos: 0</span>
+    <div className="d-flex justify-content-around mb-4">
+
+      <div className="text-center">
+        <div className="fw-bold mt-1">Pregunta</div>
+        <span className="badge bg-primary mt-1 fs-6">1/10</span>
+      </div>
+      
+      <div className="text-center">
+        <div className="fw-bold mt-1">Tiempo</div>
+        <span className="badge bg-warning text-dark mt-1 fs-6">15s</span>
+      </div>
+      
+      <div className="text-center">
+        <div className="fw-bold mt-1">Puntos</div>
+        <span className="badge bg-success mt-1 fs-6">0</span>
+      </div>
     </div>
   )
 }
