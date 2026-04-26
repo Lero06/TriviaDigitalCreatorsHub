4f62cd6483268efff62ade4276fbcc5986d59f9b
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2026-04-26 15:10:41 -0600

    Cambios en Main
    Se elimina una linea de importacion de bootstrap que se encontraba en Main, causando conflicto con los colores originales importados por el equipo de frontend. De esta forma se arregla el problema respecto a los colores del programa

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import './fixes.css';
 import App from "./App"
-import "bootstrap/dist/css/bootstrap.min.css"
 import { GameProvider } from "./context/GameContext"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
