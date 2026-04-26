b99070824b1f0919a9baec3f69f95e0470f3f8e7
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2026-04-26 01:58:27 -0600

    Cambios en Result y ResultStats
    Se conecta la logica del juego con Results y con ResultStats. En ResultStats se agrega la capacidad para recibir parametros personalizados para mostrar y en Result se agrega el hook para traer lo resultados por medio del navigate ubicado en Quiz.
    Todavia no se ha programado la funcionalidad para que se muestren las respuestas correctas, sin embargo se encuentra anotado en el manejo de proyecto como una tarea pronta a realizar.

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,31 +1,38 @@
-import CardBox from "../components/CardBox"
+import { useLocation } from "react-router-dom"
 import Btn from "../components/Btn"
 import ResultStats from "../components/ResultStats"
 
 function Result() {
+  const location = useLocation()
+  const { puntaje, respuestasCorrectas, preguntasTotales, dificultad } = location.state
   return (
     <div className="container mt-5">
 
-        <h1 className="text-center mb-4">
-          Resultados
-        </h1>
+      <h1 className="text-center mb-4">
+        Resultados
+      </h1>
 
-        <ResultStats />
+      <ResultStats
+        puntaje={puntaje}
+        respuestasCorrectas={respuestasCorrectas}
+        preguntasTotales={preguntasTotales}
+        dificultad={dificultad}
+      />
 
-        <div className="d-grid gap-2">
+      <div className="d-grid gap-2">
 
-          <Btn
-            text="Jugar Otra Vez"
-            to="/"
-            type="primary"
-          />
+        <Btn
+          text="Jugar Otra Vez"
+          to="/"
+          type="primary"
+        />
 
-          <Btn
-            text="Compartir Resultado"
-            type="success"
-          />
+        <Btn
+          text="Compartir Resultado"
+          type="success"
+        />
 
-        </div>
+      </div>
     </div>
   )
 }
