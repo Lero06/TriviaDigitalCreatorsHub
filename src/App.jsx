d529e267e84da2f9e20ae36bbfd91c84ba78322a
Joches29 <jochaveshe@est.utn.ac.cr>
2026-04-26 23:31:10 -0600

    Modificacion de App.jsx
    Se agrega el path para la nueva pagina Login. Se estructura con providers para que mantenga el contexto en todo el app, tanto de juego como de autenticazion. Ademas, con GoogleOAuthProvider y su respectivo id, podemos conectarnos a la API de Google Cloud para que nos autorize el logueo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import "bootswatch/dist/vapor/bootstrap.min.css";
 import './fixes.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { GameProvider } from "./context/GameContext"
+import { AuthProvider } from "./context/AuthContext"
 import Home from "./pages/Home"
 import Quiz from "./pages/Quiz"
 import Result from "./pages/Result"
+import Login from "./pages/Login"
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="990774059411-v93gsaevr57omvge07qqnjr0pejg8p0v.apps.googleusercontent.com">
+      <BrowserRouter>
+        <GameProvider>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/quiz" element={<Quiz />} />
+              <Route path="/result" element={<Result />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </AuthProvider>
+        </GameProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   )
 }
 
