68adabec3dffbe72189ee62e706ccac616eb82da
Joches29 <jochaveshe@est.utn.ac.cr>
2026-04-26 18:11:56 -0600

    Modificacion a GameContext.jsx
    Se añade al contexto del juego la conexion con la API. Al añadir mas hooks, mas values y la implementacion de fetchQuestions, podemos obtener las preguntas de la API segun su categoria y dificultad.

--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -1,16 +1,44 @@
 import { createContext, useContext, useState } from "react";
+import { getQuestions } from "../services/api";
 
 const GameContext = createContext()
 
 export function GameProvider({ children }) {
-    const [categoria, setCategoria] = useState("general")
+    const [categoria, setCategoria] = useState("anime")
     const [dificultad, setDificultad] = useState("easy")
+    const [questions, setQuestions] = useState([])      
+    const [loading, setLoading] = useState(false)       
+    const [error, setError] = useState(null) 
+
+     const fetchQuestions = async () => {
+        setLoading(true)           
+        setError(null) 
+    
+         try {
+            const data = await getQuestions(categoria, dificultad)
+            setQuestions(data)     
+            setError(null)         
+            
+        } catch (err) {
+            setError(err.message)
+            setQuestions([])       
+        } finally {
+            setLoading(false)
+        }
+    }
 
     const value = {
         categoria,
         setCategoria,
         dificultad,
         setDificultad,
+        questions,
+        setQuestions,
+        loading,
+        setLoading,
+        error,
+        setError,
+        fetchQuestions,
     }
 
     return (
