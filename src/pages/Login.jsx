f8d51fa23f5f1d319fc1660d8fd323a185c12fcb
Lero <lesanchezro@est.utn.ac.cr>
2026-04-29 10:01:46 -0600

    Modificacion Pagina Login
    Se modifico la pagina de Login para que estuviera centrado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,16 +19,25 @@ function Login() {
   };
 
   return (
-    <div className="container mt-5 text-center">
+    <div
+      className="container"
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        paddingTop: "150px",
+      }}
+    >
       <h1 className="mb-4">Inicia Sesión</h1>
-
-       <div 
-        style={{ 
-          display: 'flex',           
-          justifyContent: 'center',  
-          alignItems: 'center',      
-          marginBottom: '30px',
-          minHeight: '80px'          
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "30px",
+          minHeight: "80px",
         }}
       >
         <GoogleLogin
@@ -37,8 +46,7 @@ function Login() {
           size="large"
         />
       </div>
-
-      <Btn text="Volver Atrás" onClick={() => navigate(-1)} type="secondary" style={{ width: '300px', maxWidth: '100%' }}/>
+      <Btn text="Volver Atrás" onClick={() => navigate(-1)} type="secondary" style={{ width: "300px", maxWidth: "100%" }} />
     </div>
   );
 }
