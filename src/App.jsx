c4bae746bc65d92e742d167ae70ed1158ba0ca69
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2026-04-29 23:34:03 -0600

    Cambios en App y Main
    Se hacen cambios en App y Main para asegurar el correcto funcionamiento del sonido en la aplicacion.
    Se agrega una pantalla inicial en app para poder accionar el sonido manualmente cuando se navega a Home

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,51 @@ import './fixes.css';
 import { useLocation } from "react-router-dom"
 import { HashRouter, Routes, Route } from "react-router-dom"
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import { GameProvider } from "./context/GameContext"
 import { AuthProvider } from "./context/AuthContext"
 import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import { useSound2 } from "./context/SoundContext";
 import Home from "./pages/Home"
 import Quiz from "./pages/Quiz"
 import Result from "./pages/Result"
 import Login from "./pages/Login"
-import { SoundProvider } from "./context/SoundContext";
 
 function App() {
-  const [started, setStarted] = useState(false)
 
   return (
     <GoogleOAuthProvider clientId="990774059411-v93gsaevr57omvge07qqnjr0pejg8p0v.apps.googleusercontent.com">
       <HashRouter>
-        <SoundProvider>
-          <GameProvider>
-            <AuthProvider>
-              {!started ? (
-                <div
-                  onClick={() => setStarted(true)}
-                  style={{
-                    height: "100vh",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    cursor: "pointer"
-                  }}
-                >
-                  <h1>🎯 Cuestionados!</h1>
-                  <p>Tocá cualquier parte para comenzar</p>
-                </div>
-              ) : (
-                <AppRoutes />
-              )}
-            </AuthProvider>
-          </GameProvider>
-        </SoundProvider>
+        <AuthProvider>
+          <AppContent />
+        </AuthProvider>
       </HashRouter>
     </GoogleOAuthProvider>
   )
+}
+
+function AppContent() {
+  const [started, setStarted] = useState(false)
+
+  if (!started) {
+    return (
+      <div
+        onClick={() => setStarted(true)}
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          cursor: "pointer"
+        }}
+      >
+        <h1>🎯 Cuestionados!</h1>
+        <p>Tocá cualquier parte para comenzar</p>
+      </div>
+    )
+  }
+
+  return <AppRoutes />
 }
 
 function AppRoutes() {
